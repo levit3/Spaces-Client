251b5055198be0872a1eab8f955eb668d49e43bf
Nicholas Muriithi <nickstech707@gmail.com>
2024-08-05 14:31:13 +0300

    Added homepage, latestspaces components and functionalities

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar__brand">SPACES</div>
+      <ul className="navbar__links">
+        <li>Home</li>
+        <li>Offices</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <div className="navbar__auth">
+        <button className="login">Login</button>
+        <button className="signup">Signup</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
