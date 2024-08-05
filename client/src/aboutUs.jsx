5ff6877c67df740d80586c2d9ef159ecc7524c28
Benhin Mwendwa <benhinmwendwa7@gmail.com>
2024-08-05 18:41:47 +0300

    initialization

---
+++ client/src/aboutUs.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Container, Row, Col } from 'react-bootstrap';
+import Navbar from './navbar';
+import Footer from './footer';
