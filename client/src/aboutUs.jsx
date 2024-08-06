660741a964346d01b64e69098ea38c41fa689e76
Benhin Mwendwa <benhinmwendwa7@gmail.com>
2024-08-06 22:40:56 +0300

    Add image urls to About component

--- client/src/aboutUs.jsx
+++ client/src/aboutUs.jsx
@@ -2,3 +2,15 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card, Container, Row, Col } from 'react-bootstrap';
 
+
+
+const About = () => {
+    const placeholderImageUrl = 'https://via.placeholder.com/400x250.png?text=Recipe+Image';
+    const img1 = "https://www.themealdb.com/images/media/meals/vdwloy1713225718.jpg";
+    const img3 = "https://www.themealdb.com/images/media/meals/0206h11699013358.jpg";
+    const img4 = "https://www.themealdb.com/images/media/meals/jcr46d1614763831.jpg";
+    const welcomeImageUrl = "https://images.pexels.com/photos/5898313/pexels-photo-5898313.jpeg?auto=compress&cs=tinysrgb&w=800";
+
+
+}
+  export default About;
