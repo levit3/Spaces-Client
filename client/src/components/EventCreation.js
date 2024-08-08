2ab9cd96186d5c9468bd5d53d170571367ffbeeb
Favor Vincent <vfavorm77@gmail.com>
2024-08-08 22:21:01 +0300

    set image and fetched current user

--- client/src/components/EventCreation.js
+++ client/src/components/EventCreation.js
@@ -6,6 +6,19 @@ function EventCreation() {
   const [user, setUser] = useState(null);
   const [bookedSpaces, setBookedSpaces] = useState([]);
 
+  const images = {
+    main: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJjcsyLgOPmDPJOSVNXpaxCQlnPVLaQeHx4A&s"  };
+
+  useEffect(() => {
+    fetch("http://localhost:5555/api/users/92")
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching user:", error);
+      });
+  }, []);
 }
 
 export default EventCreation;
