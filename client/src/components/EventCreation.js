77da69c43656682154cb3545a7603456cbc0f1bb
Favor Vincent <vfavorm77@gmail.com>
2024-08-08 22:22:24 +0300

    fetched drop down for the locations of current user

--- client/src/components/EventCreation.js
+++ client/src/components/EventCreation.js
@@ -1,3 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import './EventCreation.css';
+
 function EventCreation() {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
@@ -20,5 +23,40 @@ function EventCreation() {
       });
   }, []);
 }
+
+useEffect(() => {
+  const fetchBookings = async () => {
+    try {
+      const response = await fetch('http://localhost:5555/api/bookings', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const bookings = await response.json();
+      const userBookings = bookings.filter(booking => booking.user_id === 92);
+      console.log(userBookings);
+
+      const uniqueSpacesMap = new Map();
+      userBookings.forEach(booking => {
+        if (!uniqueSpacesMap.has(booking.space_id)) {
+          uniqueSpacesMap.set(booking.space_id, booking.space_title);
+        }
+      });
+
+      const spaces = Array.from(uniqueSpacesMap, ([id, title]) => ({
+        id,
+        title
+      }));
+
+      console.log(spaces);
+      setBookedSpaces(spaces);
+      setLoading(true)
+    } catch (error) {
+      console.error('Error fetching bookings:', error);
+    }
+  };
+  fetchBookings();
+}, [user]);
 
 export default EventCreation;
