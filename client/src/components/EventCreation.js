617701261cab31e7f548eb1fa7d494977555ef08
Favor Vincent <vfavorm77@gmail.com>
2024-08-08 22:20:11 +0300

    added states for event creation

---
+++ client/src/components/EventCreation.js
@@ -0,0 +1,11 @@
+function EventCreation() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [date, setDate] = useState('');
+  const [spaceId, setSpaceId] = useState('');
+  const [user, setUser] = useState(null);
+  const [bookedSpaces, setBookedSpaces] = useState([]);
+
+}
+
+export default EventCreation;
