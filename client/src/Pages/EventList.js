fc7e4f76d4b800579dba52d5e05156c3ed305782
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-08-09 20:20:51 +0300

    implements react-date picket to filter events by date

--- client/src/Pages/EventList.js
+++ client/src/Pages/EventList.js
@@ -1,10 +1,35 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import "./events-calendarwith-image.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function EventList() {
   const [filteredEvents, setFilteredEvents] = useState([]);
   const [events, setEvents] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const [selectedDate, setSelectedDate] = useState(() => {
+    const storedDate = localStorage.getItem("selectedDate");
+    return storedDate ? new Date(storedDate) : new Date();
+  });
+
+  useEffect(() => {
+    localStorage.setItem("selectedDate", selectedDate);
+  }, [selectedDate]);
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  const filterEventsByDate = () => {
+    const filteredEvents = events.filter(
+      (event) =>
+        new Date(event.date).toLocaleDateString("en-US") ===
+        new Date(selectedDate).toLocaleDateString("en-US")
+    );
+    setFilteredEvents(filteredEvents);
+  };
 
   useEffect(() => {
     fetch("http://127.0.0.1:5555/api/events")
@@ -19,6 +44,7 @@ function EventList() {
   if (loading) {
     return <div>Loading...</div>;
   }
+
   return (
     <div className="events-container">
       <header className="events-header">
@@ -27,6 +53,13 @@ function EventList() {
 
       <section className="events-schedule">
         <div className="events-filter">
+          <div className="date-picker">
+            <DatePicker
+              selected={selectedDate}
+              onChange={handleDateChange}
+              dateFormat="d MMMM, yyyy"
+            />
+          </div>
           <hr />
         </div>
 
