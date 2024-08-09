05dbe58a3cf7e9340cdb7150f70feb16ad78972b
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-08-09 20:38:50 +0300

    adds styling to the event details page

--- client/src/Pages/EventDetail.js
+++ client/src/Pages/EventDetail.js
@@ -5,6 +5,7 @@ function EventPage() {
   const { id } = useParams();
   const [event, setEvent] = useState({});
   const [loading, setLoading] = useState(true);
+  const time = "8AM - 4PM";
   const src =
     "https://cdn.builder.io/api/v1/image/assets/TEMP/dfe3240009fd425ddb9fe0e226d9444abbf3e86a4d8c9e87b75a5e5e7efef0f6?apiKey=8ad21786488a40e8a18ed0f9f1e05271&&apiKey=8ad21786488a40e8a18ed0f9f1e05271";
 
