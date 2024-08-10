573b401da35d02688ee8a0b12fdb4f021e7cbcbb
Benhin Mwendwa <benhinmwendwa7@gmail.com>
2024-08-11 01:42:45 +0300

    initialize space form components

--- client/src/TenantPage/SpaceForm.js
+++ client/src/TenantPage/SpaceForm.js
@@ -1,3 +1,10 @@
 import React from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+const SpaceForm = ({ space, onClose, onSuccess }) => {
+    const initialValues = space || {
+        title: '',
+        description: '',
+        location: '',
+        price_per_hour: '',
+        status: 'available'
