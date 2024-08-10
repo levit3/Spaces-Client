1b4a8d1cbf346be56e1ec1b45067c7ad6388dc2d
Benhin Mwendwa <benhinmwendwa7@gmail.com>
2024-08-10 23:10:07 +0300

    Add state variables for user list,editing user and form data

--- client/src/TenantPage/ManageUsers.js
+++ client/src/TenantPage/ManageUsers.js
@@ -1,4 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Table, Button, Container } from 'react-bootstrap';
-import UserModal from './UserModal'; 
+import { Table, Button, Container, Form, InputGroup } from 'react-bootstrap';
+
+const ManageUsers = () => {
+    const [users, setUsers] = useState([]);
+    const [editingUser, setEditingUser] = useState(null);
+    const [formData, setFormData] = useState({ name: '', email: '', role: '' });
+  
