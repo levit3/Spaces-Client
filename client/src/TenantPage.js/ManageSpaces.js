893a2446fac1d351825373f8ee2de116090ab504
Benhin Mwendwa <benhinmwendwa7@gmail.com>
2024-08-10 19:33:53 +0300

    initialize managespaces component with state variables

--- client/src/TenantPage.js/ManageSpaces.js
+++ client/src/TenantPage.js/ManageSpaces.js
@@ -1,3 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import SpaceForm from './SpaceForm';
+
+const ManageSpaces = () => {
+    const [spaces, setSpaces] = useState([]);
+    const [selectedSpace, setSelectedSpace] = useState(null);
