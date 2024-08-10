f220cb6d3ba5b89d603bf324dc8639669fdb4daa
Benhin Mwendwa <benhinmwendwa7@gmail.com>
2024-08-10 19:35:35 +0300

    fetch spaces data from API

--- client/src/TenantPage.js/ManageSpaces.js
+++ client/src/TenantPage.js/ManageSpaces.js
@@ -5,3 +5,10 @@ import SpaceForm from './SpaceForm';
 const ManageSpaces = () => {
     const [spaces, setSpaces] = useState([]);
     const [selectedSpace, setSelectedSpace] = useState(null);
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:5555/api/users/7/')
+            .then(response => setSpaces(response.data.spaces))
+            .catch(error => console.error('Error fetching spaces:', error));
+    }, []);
+
