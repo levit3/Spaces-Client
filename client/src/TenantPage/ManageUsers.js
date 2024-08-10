c6248385442fda453e8a5239b615e308d6fc9327
Benhin Mwendwa <benhinmwendwa7@gmail.com>
2024-08-10 23:47:52 +0300

    render user data in table rows

--- client/src/TenantPage/ManageUsers.js
+++ client/src/TenantPage/ManageUsers.js
@@ -101,10 +101,16 @@ const ManageUsers = () => {
             <th>Name</th>
             <th>Email</th>
             <th>Role</th>
-           
           </tr>
         </thead>
         <tbody>
+        {users.map(user => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.role}</td>
+              <td>
 
 
     
