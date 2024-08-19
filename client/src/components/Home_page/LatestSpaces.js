51de13506f913254bc246f456c41a20c928d8623
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-08-19 12:01:16 +0300

    finishing touches on the homepage

--- client/src/components/Home_page/LatestSpaces.js
+++ client/src/components/Home_page/LatestSpaces.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./Homepage.css";
+import { Navigate } from "react-router-dom";
 const API = process.env.REACT_APP_SERVER_API;
 
 function LatestSpaces() {
   const [properties, setProperties] = useState([]);
   const [loading, setLoading] = useState(true);
+  const navigate = Navigate();
 
   useEffect(() => {
     fetch(`${API}/api/spaces`, {
@@ -37,7 +39,11 @@ function LatestSpaces() {
       <h2>Latest Listed Spaces</h2>
       <div className="properties__list">
         {properties.slice(0, 5).map((property, index) => (
-          <div key={index} className="property">
+          <div
+            key={index}
+            className="property"
+            onClick={() => navigate(`/space/${property.id}`)}
+          >
             <img src={property.space_images[0].image_url} alt={property.city} />
             <div className="property__info">
               <h3>{property.price_per_hour}</h3>
